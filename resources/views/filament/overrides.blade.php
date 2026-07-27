5b913c07e22aaac5d81eb600e9154be360d69f72
Muhammad Ihsan Anwar <muhammadihsananwar3@gmail.com>
2026-07-27 22:30:46 +0700

    Style: target samakan-domisili-checkbox with specific overrides

--- resources/views/filament/overrides.blade.php
+++ resources/views/filament/overrides.blade.php
@@ -392,3 +392,18 @@ input.fi-checkbox-input:disabled {
   opacity: 0.95 !important;
 }
 </style>
+<style>
+/* Specific target for the Samakan dengan Domisili checkbox input */
+input.samakan-domisili-checkbox {
+  background-color: #FAF3EB !important;
+  border-radius: .5rem !important;
+  border: 1px solid rgba(62,39,26,0.08) !important;
+}
+
+input.samakan-domisili-checkbox:checked {
+  background-color: #3E271A !important;
+  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg viewBox='0 0 16 16' fill='%23fff' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12.207 4.793a1 1 0 0 1 0 1.414l-5 5a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L6.5 9.086l4.293-4.293a1 1 0 0 1 1.414 0z'/%3E%3C/svg%3E") !important;
+  background-repeat: no-repeat !important;
+  background-position: center !important;
+}
+</style>
